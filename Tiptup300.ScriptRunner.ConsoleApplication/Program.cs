6bd92f9a6b58ab72fbbf747f4fc0560a7d6eefc8
Matthew Deweese <tiptup300@gmail.com>
2025-01-19 09:13:40 -0500

    Add project files.

---
+++ Tiptup300.ScriptRunner.ConsoleApplication/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tiptup300.ScriptRunner.ConsoleApplication
+{
+   internal class Program
+   {
+      static void Main(string[] args)
+      {
+         Console.WriteLine("Hello, World!");
+      }
+   }
+}
